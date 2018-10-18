926875a87debdb3732adbeac9b01abd5e57b30eb
eric.fennis@mediamonks.com <eric.fennis@mediamonks.com>
2018-10-18 15:14:39 +0200

    Add the Vue Structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,55 @@
+// http://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
+  env: {
+    browser: true,
+    node:true
+  },
+  extends: ['airbnb-base', 'prettier', 'plugin:vue/recommended', 'plugin:vue-types/strongly-recommended'],
+  plugins: ['import', 'prettier', 'vue'],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build-tools/config/webpack/webpack.base.conf.js',
+      },
+    },
+  },
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+        ts: 'never',
+      },
+    ],
+    // allow debugger during development
+    'no-debugger': 2,
+    'no-console': 1,
+    'no-param-reassign': 0,
+    // only for use with getter-setters
+    'no-underscore-dangle': 0,
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, trailingComma: 'all', printWidth: 100, tabWidth: 2 },
+    ],
+  },
+  overrides: [
+    {
+      "files": [ "src/**/*.vue"],
+      "rules": {
+        'prettier/prettier': [
+          'off'
+        ],
+      },
+    },
+  ],
 };
