0488092eecd526749efb867e273a9b719f0cd99b
Eric Fennis <eric.fennis@gmail.com>
2018-11-26 22:59:08 +0100

    Added scss variables, mixins and functions globally imported

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 // vue.config.js
 module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/assets/styles/partials/_variables.scss";
+          @import "@/assets/styles/modules/_functions.scss";
+          @import "@/assets/styles/modules/_mixins.scss";
+        `,
+      }
+    }
+  },
   chainWebpack: config => {
+    // SVG Loader
     const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
     svgRule
