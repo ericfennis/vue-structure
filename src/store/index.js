52d08528f7f9e0cf9eb1dc7116d270386d3bd9a3
Eric Fennis <eric.fennis@gmail.com>
2018-12-09 15:20:05 +0100

    Renamed router file from index.js to router.js and renamed store file from indexjs to store.js, minor changes in .env file

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,3 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import store from './store';
 
-import * as modules from './modules';
-
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  modules,
-});
+export default store;
