ee4b6c3ebc61ae076f56f33dde2d2b72bfe3e4bc
Eric Fennis <eric.fennis@mediamonks.com>
2018-12-24 12:49:34 +0100

    Formated the start modal html shorter

--- src/components/ExampleComponent/ExampleComponent.vue
+++ src/components/ExampleComponent/ExampleComponent.vue
@@ -8,11 +8,8 @@
 
     <!-- DELETE ME -->
     <figure style="border-radius: 8px; margin: 12vh auto; width: 480px; background: #fafafa; box-shadow: 0 3px 6px rgba(0,0,0,.16); padding:45px 35px; box-sizing: border-box; text-align: center;">
-      <img 
-        src="https://raw.githubusercontent.com/ericfennis/vue-structure/4bff88983d079a288be98b0adcb5cc00e43cecc7/src/assets/images/vue-structure.png" 
-        alt="Vue Structure Logo"
-        style="margin-bottom:35px"
-      >
+      <!-- eslint-disable vue/max-attributes-per-line -->
+      <img src="https://raw.githubusercontent.com/ericfennis/vue-structure/4bff88983d079a288be98b0adcb5cc00e43cecc7/src/assets/images/vue-structure.png" alt="Vue Structure Logo" style="margin-bottom:35px">
       <figcaption style="display: flex; flex-shrink: 0; flex-grow:2; flex-basis: 80px; flex-direction: column;">
         <div style="display: flex; justify-content: space-around; margin-bottom: 20px;">
           <a href="https://ericfennis.gitbook.io/vue-structure/">Vue Structure docs</a>
