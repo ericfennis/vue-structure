13382269320e2c9f33363ccc5c0986f39a009105
Eric Fennis <eric.fennis@mediamonks.com>
2018-12-24 10:51:12 +0100

    Added babel config for short import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: ['@vue/app'],
+  plugins: [
+    [
+      'transform-imports',
+      {
+        '@/components': {
+          transform: '@/components/${member}',
+          preventFullImport: true,
+        },
+      },
+    ],
+  ],
 };
